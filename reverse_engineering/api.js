c1dc48dd88416927872462fa00ce9d4d4aeedf97
Dmytro <beardimon2001@gmail.com>
2022-09-21 08:01:40 +0300

    add adapter for jsonSchema

--- reverse_engineering/api.js
+++ reverse_engineering/api.js
@@ -6,6 +6,7 @@ const wrapFieldsIntoJSONSchema = require('./helpers/wrapFieldsIntoJSONSchema');
 const includeMetadataInJSONSchema = require('./helpers/includeMetadataInJSONSchema');
 const pipe = require('../helpers/pipe');
 const path = require('path');
+const adaptJsonSchema = require('./helpers/adaptJsonSchema/adaptJsonSchema');
 
 module.exports = {
 	async reFromFile(data, logger, callback) {
@@ -33,5 +34,27 @@ module.exports = {
 			logger.log('error', errorObject, 'Parquet Reverse-Engineering Error');
 			callback(errorObject);
 		}
-	}
+	},
+
+	adaptJsonSchema(data, logger, callback) {
+		logger.log('info', 'Adaptation of JSON Schema started...', 'Adapt JSON Schema');
+		try {
+			const jsonSchema = JSON.parse(data.jsonSchema);
+
+			const adaptedJsonSchema = adaptJsonSchema(jsonSchema);
+
+			logger.log('info', 'Adaptation of JSON Schema finished.', 'Adapt JSON Schema');
+
+			callback(null, {
+				jsonSchema: JSON.stringify(adaptedJsonSchema)
+			});
+		} catch(e) {
+			const errorObject = {
+				message: `${error.message}\nFile name: ${fileName}`,
+				stack: error.stack,
+			};
+			logger.log('error', errorObject, 'Adaptation of JSON Schema Error');
+			callback(errorObject);
+		}
+	},
 };
